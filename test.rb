7755663999f3ce15a60424256b7e4e6290cfec22
Sophia Kapchinsky <s.kapchinsky@gmail.com>
2023-11-09 01:50:08 +0000

    W2D2 adding functionality in Ruby

---
+++ test.rb
@@ -0,0 +1,11 @@
+puts "this is a string"
+puts 10 +18
+puts false
+puts 3>2
+greeting = "Hello"
+full_name = "Sophia Kapchinsky"
+
+puts greeting + " " + full_name + "!"
+
+puts "Hello there #(full_name), how are you?"
+puts "Hello there #{full_name}, how are you?"
